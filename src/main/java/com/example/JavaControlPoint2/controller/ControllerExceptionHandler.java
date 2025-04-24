f3802a491b4e323c524c1e9e1ba4f9352727dd3c
Arthur <karla-an@mail.ru>
2025-04-24 21:10:32 +0700

    Убрал implementation 'org.springframework:spring-webflux:6.2.3'

--- src/main/java/com/example/JavaControlPoint2/controller/ControllerExceptionHandler.java
+++ src/main/java/com/example/JavaControlPoint2/controller/ControllerExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -20,21 +19,6 @@ import java.util.Map;
 @Slf4j
 public class ControllerExceptionHandler {
 
-//    @ExceptionHandler(ResponseStatusException.class)
-//    public ResponseEntity<Object> resolveResponseStatusException(ResponseStatusException ex, HttpServletRequest request) {
-//        return buildErrorResponse(ex.getStatusCode().value(), ex.getReason(), request);
-//    }
-//
-//    @ExceptionHandler(UnauthorizedException.class)
-//    public ResponseEntity<Object> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request) {
-//        return buildErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), request);
-//    }
-//
-//    @ExceptionHandler(ForbiddenException.class)
-//    public ResponseEntity<Object> handleForbiddenException(ForbiddenException ex, HttpServletRequest request) {
-//        return buildErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage(), request);
-//    }
-
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
         return buildErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request);
@@ -44,24 +28,6 @@ public class ControllerExceptionHandler {
     public ResponseEntity<Object> handleGenericException(Exception ex, HttpServletRequest request) {
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error occurred", request);
     }
-
-//    @ExceptionHandler(WebClientResponseException.class)
-//    public ResponseEntity<Object> handleWebClientResponseException(WebClientResponseException ex, HttpServletRequest request) {
-//        try {
-//            // Парсим JSON-ответ от AuthService
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            Map<String, Object> originalResponse = objectMapper.readValue(ex.getResponseBodyAsString(), Map.class);
-//
-//            // Логируем оригинальный ответ
-//            log.error("Ошибка от внешнего сервиса: {}", originalResponse);
-//
-//            return ResponseEntity.status(ex.getStatusCode()).body(originalResponse);
-//        } catch (Exception parseEx) {
-//            log.error("Не удалось обработать JSON-ответ от AuthService", parseEx);
-//            return buildErrorResponse(ex.getStatusCode().value(), ex.getMessage(), request);
-//        }
-//    }
-
 
     private ResponseEntity<Object> buildErrorResponse(int status, String message, HttpServletRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
